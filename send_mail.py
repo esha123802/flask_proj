62e6edab83fd8d006af85be64e1a4f90cdd935bc
esha123802 <76272081+esha123802@users.noreply.github.com>
2021-07-19 17:20:48 +0530

    EMAIL CHANGE

--- send_mail.py
+++ send_mail.py
@@ -3,38 +3,38 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 
-# def send_mail(name, phone, email, comments):
-#     port = 2525
-#     smtp_server = 'smtp.mailtrap.io'
-#     login = 'b37a81e73f2874'
-#     password = '5d1fa2dd3edcd1'
-#     message = f"<h3>New Contact Me Submission</h3><ul><li>Name: {name}</li><li>Phone: {phone}</li><li>Email: {email}</li><li>Comments: {comments}</li></ul>"
-#     sender_email = 'ex1@gmail.com'
-#     receiver_email = 'ex2@gmail.com'
-#     ms g = MIMEText(message, 'html')
-#     msg['Subject'] = 'Contact Me'
-#     msg['From'] = sender_email
-#     msg['To'] = receiver_email
-
-#     # Send email
-#     with smtplib.SMTP(smtp_server, port) as server:
-#         server.login(login, password)
-#         server.sendmail(sender_email, receiver_email, msg.as_string())
-
-EMAIL_PWD = '1238@Lala'
 def send_mail(name, phone, email, comments):
-    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'b37a81e73f2874'
+    password = '5d1fa2dd3edcd1'
+    message = f"<h3>New Contact Me Submission</h3><ul><li>Name: {name}</li><li>Phone: {phone}</li><li>Email: {email}</li><li>Comments: {comments}</li></ul>"
+    sender_email = 'ex1@gmail.com'
+    receiver_email = 'ex2@gmail.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'Contact Me'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
 
-        smtp.login('lalalolo.2469@gmail.com', EMAIL_PWD)
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
 
-        message = f"<h3>New Contact Me Submission</h3><ul><li>Name: {name}</li><li>Phone: {phone}</li><li>Email: {email}</li><li>Comments: {comments}</li></ul>"
+# EMAIL_PWD = '1238@Lala'
+# def send_mail(name, phone, email, comments):
+#     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+#         smtp.ehlo()
+#         smtp.starttls()
+#         smtp.ehlo()
 
-        msg = MIMEText(message, 'html')
-        msg['Subject'] = 'Contact Me'
-        msg['From'] = 'lalalolo.2469@gmail.com'
-        msg['To'] = 'lalalolo.2469@gmail.com'
+#         smtp.login('lalalolo.2469@gmail.com', EMAIL_PWD)
 
-        smtp.sendmail('lalalolo.2469@gmail.com', 'lalalolo.2469@gmail.com', msg.as_string())
+#         message = f"<h3>New Contact Me Submission</h3><ul><li>Name: {name}</li><li>Phone: {phone}</li><li>Email: {email}</li><li>Comments: {comments}</li></ul>"
+
+#         msg = MIMEText(message, 'html')
+#         msg['Subject'] = 'Contact Me'
+#         msg['From'] = 'lalalolo.2469@gmail.com'
+#         msg['To'] = 'lalalolo.2469@gmail.com'
+
+#         smtp.sendmail('lalalolo.2469@gmail.com', 'lalalolo.2469@gmail.com', msg.as_string())
